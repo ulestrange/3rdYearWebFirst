bda5ec037d253f6928424306ab1688136bd33731
Una LEstrange <lestrange.una@itsligo.ie>
2020-09-10 11:10:49 +0100

    Endpoint of post book added which just pushes the book onto an array

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const app = express();
 
 const port = 3000;
 
+
+// Configuring body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+
+let books = [];
+
+
+
+
+
+app.get('/', (req, res) =>
+  res.send('hello world, Una is using Express'));
+
+app.get('/bananas', (req, res) =>
+  res.send('hello world, this is bananas'));
+
+
+  app.post('/book', (req, res) => {
+      const book = req.body;
+      books.push(book);
+
+      res.send ('book has been added to the database');
+      console.log(`book name is ${book.name} number of book is ${books.length}`);
+
+  });
 
 app.listen(port,  () => console.log(`Example app listening on 
   ${port}!`))
