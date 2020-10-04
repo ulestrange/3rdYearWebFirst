9e6cd84cc5a9d0fff74f5fff2066411f7ec82598
Una LEstrange <lestrange.una@itsligo.ie>
2020-10-04 14:55:20 +0100

    task2 finished with all routes done

--- routes/books.js
+++ routes/books.js
@@ -4,5 +4,33 @@ import express from 'express';
 
 const router = express.Router();
 
+let books = [];
+
+router.post('/', (req, res) => {
+    const book = req.body;
+    books.push(book);
+
+    res.send ('book has been added to the database');
+    console.log(`book name is ${book.name} number of book is ${books.length}`);
+
+});
+
+router.get('/', (req, res) => {
+    res.send(books);
+})
+
+router.get('/:id', (req,res) => {
+
+   let id = req.params.id;
+    res.json(books[id]);
+})
+
+router.delete('/:id',(req, res) => {
+  let id = req.params.id; 
+  console.log(`removing book ${books[id].name}`)
+  books.splice(req.params.id, 1);
+  res.send(books);
+
+})
 
 export default router;
