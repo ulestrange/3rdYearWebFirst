e655e0a5c0d8bca2a4f716136f4337c48eee10c2
Una LEstrange <lestrange.una@itsligo.ie>
2020-11-09 09:23:48 +0000

    cors options

--- index.js
+++ index.js
@@ -6,7 +6,18 @@ import mongoose from 'mongoose';
 
 const app = express();
 
-app.use(cors());
+// to allow requests from different origins
+// for the /books routes only.
+
+
+// for example purposes only this will only
+// allow access from a specific origin.
+// to allow access from anywhere leave the options blank
+let corsOptions = {
+  origin: 'http://localhost:4200'
+}
+
+app.use('/books',cors(corsOptions));
 
 const port = 3000;
 
